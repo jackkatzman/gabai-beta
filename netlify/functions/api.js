b0f73d57daa1772d9dcde8f2fa6f73bae3e0a170
jackkatzman <jack@symcousa.com>
2025-09-21 13:25:35 -0400

    chore(netlify): remove legacy api function (site is static)

--- netlify/functions/api.js
+++
@@ -1,7 +0,0 @@
-const serverless = require('serverless-http');
-
-// Import the Express app (default export from TypeScript)
-const { default: app } = require('../../dist/index.js');
-
-// Export the serverless handler
-exports.handler = serverless(app);
